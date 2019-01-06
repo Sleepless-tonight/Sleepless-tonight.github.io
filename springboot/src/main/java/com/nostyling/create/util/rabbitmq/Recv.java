6861a9e662388618f324bdde5a252ac7d28a255d
shiliang <Shiliang.1995>
2019-01-06 21:13:04 +0800

    201916211241

---
+++ springboot/src/main/java/com/nostyling/create/util/rabbitmq/Recv.java
@@ -0,0 +1,34 @@
+package com.nostyling.create.util.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Recv {
+
+    private final static String HOST = "192.168.100.220";
+    private final static Integer PORT = 5672;
+    private final static String USERNAME = "admin";
+    private final static String PASSWORD = "5672";
+    private final static String QUEUE_NAME = "deliver_order_dts";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername(USERNAME);
+        factory.setPassword(PASSWORD);
+        factory.setHost(HOST);
+        factory.setPort(PORT);
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+}
