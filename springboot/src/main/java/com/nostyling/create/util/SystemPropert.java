4018029bfaeb7a1a17dc9e82f529204d1193f2a9
shiliang <Shiliang.1995>
2019-01-10 17:20:21 +0800

    2019110172011

--- springboot/src/main/java/com/nostyling/create/util/SystemPropert.java
+++ springboot/src/main/java/com/nostyling/create/util/SystemPropert.java
@@ -15,8 +15,18 @@ public class SystemPropert {
         Properties properties = System.getProperties();
         for (String s :properties.stringPropertyNames()
                 ) {
-            System.out.println(s+": " +properties.getProperty(s));
+            System.out.println(s+" -> " +properties.getProperty(s));
         }
-        //properties.stringPropertyNames().forEach(System.out::println);
+        System.out.println("***************************************************");
+        properties.stringPropertyNames().forEach(System.out::println);
+        System.out.println("***************************************************");
+        properties.list(System.out);
+        Runtime rt = Runtime.getRuntime();
+        System.out.println("Total Memory = "
+                + rt.totalMemory()
+                + " Free Memory = "
+                + rt.freeMemory());
     }
+
 }
+
