7d2e323187e3d99aa77480dfc1d857567762770b
shiliang <Shiliang.1995>
2019-06-27 22:17:18 +0800

    2019627221708

---
+++ html/technology/SampleMarkdown.mk
@@ -0,0 +1,100 @@
+# 这是标题、
+## 这是二级标题、
+### 这是三级标题
+
+---
+
+> 引用
+> > 嵌套引用
+> > >三嵌套引用
+> > > > 四嵌套引用
+
+---
+- -
+  - 
+- - 
+  -
+1. 1. 
+   2.
+      1.
+2. 1. 
+   2. 
+---
+
+![image](https://nostyling-1256016577.cos.ap-beijing.myqcloud.com/GitHub_cat.png)
+
+[GitHub](http://github.com)
+自动生成连接  <http://www.github.com/>
+
+---
+
+```java
+//代码块
+```
+
+---
+
+```math
+E = mc^2
+```
+
+---
+表格：
+
+header 1 | header 2
+---|---
+row 1 col 1 | row 1 col 2
+row 2 col 1 | row 2 col 2
+
+---
+
+<html>
+<!--在这里插入内容-->
+<p>在这里插入 HTML 内容</p>
+</html>
+
+---
+制作待办事项 To-do Lis
+- [ ] 待办事项
+    - [ ] 待办事项
+- [x] 待办事项
+    - [x] 待办事项
+
+---
+绘制：流程图、序列图、甘特图、表格
+
+流程图：
+
+```
+graph LR
+A-->B
+```
+
+
+```
+graph TD
+A-->B
+```
+
+序列图：
+
+```
+sequenceDiagram
+A->>B: How are you?
+B->>A: Great!
+```
+
+甘特图：
+
+```
+gantt
+dateFormat YYYY-MM-DD
+section S1
+T1: 2014-01-01, 9d
+section S2
+T2: 2014-01-11, 9d
+section S3
+T3: 2014-01-02, 9d
+```
+
+---
