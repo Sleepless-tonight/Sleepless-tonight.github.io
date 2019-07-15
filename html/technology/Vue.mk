0d4d4812bb75a5c244652a3452678e153c23e6df
shiliang <Shiliang.1995>
2019-07-15 22:51:24 +0800

    2019715225113

--- html/technology/Vue.mk
+++ html/technology/Vue.mk
@@ -1,3 +1,15 @@
 # Vue 学习笔记
 
 ### 1、node.js下载、安装略过
+
+npm cache clear -force清空缓存
+
+根据install.js，脚本通过http.get下载chromediriver二进制文件。
+但是zip文件已被移动，脚本不处理这种情况。
+
+npm install chromedriver --chromedriver_cdnurl=http://cdn.npm.taobao.org/dist/chromedriver
+
+npm audit
+npm audit fix
+
+(use `npm audit fix --force` to install breaking changes; or refer to `npm audit` for steps to fix these manually)
