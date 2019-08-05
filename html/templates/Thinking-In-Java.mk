fa1c26c3930c490eabbc3ad3d6f0212321c281da
shiliang <Shiliang.1995>
2019-08-05 19:03:34 +0800

    201985190318

--- html/templates/Thinking-In-Java.mk
+++ html/templates/Thinking-In-Java.mk
@@ -1053,6 +1053,273 @@ public class FinalizeEscapeGC {
 ```
 
 #### 4.4 成员初始化
+###### 一个类的所有基本类型数据成员都会保证获得一个初始值。若被定义成相对于一个方法的“局部”变量，这一保证就通过编译期的出错提示表现出来。
+
+##### 4.4.1 规定初始化
+###### 想自己为变量赋予一个初始值，直接的做法是在类内部定义变量的同时也为其赋值。
+
+##### 4.4.2 构建器初始化
+###### 可考虑用构建器执行初始化进程。这样便可在编程时获得更大的灵活程度。要注意这样一件事情：不可妨碍自动初始化的进行，它在构建器进入之前就会发生。例如：
+```java
+class Counter {
+int i;
+Counter() { i = 7; }
+// . . .
+```
+###### 那么i首先会初始化成零，然后变成 7。对于所有基本类型以及对象句柄，这种情况都是成立的，其中包括在定义时已进行了明确初始化的那些一些。
+
+###### 1. 初始化顺序
+###### 在一个类里，初始化的顺序是由变量在类内的定义顺序决定的。即使变量定义大量遍布于方法定义的中间，那些变量仍会在调用任何方法之前得到初始化——甚至在构建器调用之前。例如：
+```java
+//: OrderOfInitialization.java
+// Demonstrates initialization order.
+
+// When the constructor is called, to create a
+// Tag object, you'll see a message:
+class Tag {
+  Tag(int marker) {
+    System.out.println("Tag(" + marker + ")");
+  }
+}
+
+class Card {
+  Tag t1 = new Tag(1); // Before constructor
+  Card() {
+    // Indicate we're in the constructor:
+    System.out.println("Card()");
+    t3 = new Tag(33); // Re-initialize t3
+  }
+  Tag t2 = new Tag(2); // After constructor
+  void f() {
+    System.out.println("f()");
+  }
+  Tag t3 = new Tag(3); // At end
+}
+
+public class OrderOfInitialization {
+  public static void main(String[] args) {
+    Card t = new Card();
+    t.f(); // Shows that construction is done
+  }
+} ///:~
+```
+
+###### 它的输入结果如下：
+```java
+Tag(1)
+Tag(2)
+Tag(3)
+Card()
+Tag(33)
+f()
+```
+###### 2. 静态数据的初始化
+###### 若数据是静态的（static），那么同样的事情就会发生；如果它属于一个基本类型（主类型），而且未对其初始化，就会自动获得自己的标准基本类型初始值；如果它是指向一个对象的句柄，那么除非新建一个对象，并将句柄同它连接起来，否则就会得到一个空值（NULL）。
+###### static 值只有一个存储区域。如果它属于一个基本类型（主类型），而且未对其初始化，就会自动获得自己的标准基本类型初始值；如果它是指向一个对象的句柄，那么除非新建一个对象，并将句柄同它连接起来，否则就会得到一个空值（NULL）。
+```java
+//: StaticInitialization.java
+// Specifying initial values in a
+// class definition.
+
+class Bowl {
+  Bowl(int marker) {
+    System.out.println("Bowl(" + marker + ")");
+  }
+  void f(int marker) {
+    System.out.println("f(" + marker + ")");
+  }
+}
+
+class Table {
+  static Bowl b1 = new Bowl(1);
+  Table() {
+    System.out.println("Table()");
+    b2.f(1);
+  }
+  void f2(int marker) {
+    System.out.println("f2(" + marker + ")");
+  }
+  static Bowl b2 = new Bowl(2);
+}
+
+class Cupboard {
+  Bowl b3 = new Bowl(3);
+  static Bowl b4 = new Bowl(4);
+  Cupboard() {
+    System.out.println("Cupboard()");
+    b4.f(2);
+  }
+  void f3(int marker) {
+    System.out.println("f3(" + marker + ")");
+  }
+  static Bowl b5 = new Bowl(5);
+}
+
+public class StaticInitialization {
+  public static void main(String[] args) {
+    System.out.println(
+      "Creating new Cupboard() in main");
+    new Cupboard();
+    System.out.println(
+      "Creating new Cupboard() in main");
+    new Cupboard();
+    t2.f2(1);
+    t3.f3(1);
+  }
+  static Table t2 = new Table();
+  static Cupboard t3 = new Cupboard();
+} ///:~
+```
+
+###### Bowl允许我们检查一个类的创建过程，而Table和Cupboard能创建散布于类定义中的Bowl的static成员。注意在static定义之前，Cupboard先创建了一个非static的Bowl b3。它的输出结果如下：
+
+```java
+Bowl(1)
+Bowl(2)
+Table()
+f(1)
+Bowl(4)
+Bowl(5)
+Bowl(3)
+Cupboard()
+f(2)
+Creating new Cupboard() in main
+Bowl(3)
+Cupboard()
+f(2)
+Creating new Cupboard() in main
+Bowl(3)
+Cupboard()
+f(2)
+f2(1)
+f3(1)
+```
+
+###### static 初始化只有在必要的时候才会进行。如果不创建一个 Table 对象，而且永远都不引用 Table.b1或 Table.b2，那么 static Bowl  b1和 b2永远都不会创建。然而，只有在创建了第一个 Table 对象之后（或者发生了第一次 static 访问），它们才会创建。在那以后，static对象不会重新初始化。 初始化的顺序是首先 static（如果它们尚未由前一次对象创建过程初始化），接着是非 static对象。大家可从输出结果中找到相应的证据。
+
+###### 总结一下对象的创建过程。请考虑一个名为Dog的类：
+
+1. 类型为Dog的一个对象首次创建时，或者Dog类的static方法／static字段首次访问时，Java解释器必须找到Dog.class（在事先设好的类路径里搜索）。
+
+2. 找到Dog.class后（它会创建一个Class对象，这将在后面学到），它的所有static初始化模块都会运行。因此，static初始化仅发生一次——在Class对象首次载入的时候。
+
+3. 创建一个new Dog()时，Dog对象的构建进程首先会在内存堆（Heap）里为一个Dog对象分配足够多的存储空间。
+
+4. 这种存储空间会清为零，将Dog中的所有基本类型设为它们的默认值（零用于数字，以及boolean和char的等价设定）。
+
+5. 进行字段定义时发生的所有初始化都会执行。
+
+6. 执行构建器。正如第6章将要讲到的那样，这实际可能要求进行相当多的操作，特别是在涉及继承的时候。
+
+###### 1. 明确进行的静态初始化
+###### Java允许我们将其他static初始化工作划分到类内一个特殊的“static构建从句”（有时也叫作“静态块”）里。它看起来象下面这个样子：
+
+```java
+class Spoon {
+  static int i;
+  static {
+    i = 47;
+  }
+}
+  // . . .
+```
+###### 尽管看起来象个方法，但它实际只是一个static关键字，后面跟随一个方法主体。与其他static初始化一样，这段代码仅执行一次——首次生成那个类的一个对象时，或者首次访问属于那个类的一个static成员时（即便从未生成过那个类的对象）。例如：
+```java
+//: ExplicitStatic.java
+// Explicit static initialization
+// with the "static" clause.
+
+class Cup {
+  Cup(int marker) {
+    System.out.println("Cup(" + marker + ")");
+  }
+  void f(int marker) {
+    System.out.println("f(" + marker + ")");
+  }
+}
+
+class Cups {
+  static Cup c1;
+  static Cup c2;
+  static {
+    c1 = new Cup(1);
+    c2 = new Cup(2);
+  }
+  Cups() {
+    System.out.println("Cups()");
+  }
+}
+
+public class ExplicitStatic {
+  public static void main(String[] args) {
+    System.out.println("Inside main()");
+    Cups.c1.f(99);  // (1)
+  }
+  static Cups x = new Cups();  // (2)
+  static Cups y = new Cups();  // (2) 
+} ///:~
+```
+
+###### 2. 非静态实例的初始化
+###### 针对每个对象的非静态变量的初始化，Java 1.1提供了一种类似的语法格式。下面是一个例子：
+```java
+//: Mugs.java
+// Java 1.1 "Instance Initialization"
+
+class Mug {
+  Mug(int marker) {
+    System.out.println("Mug(" + marker + ")");
+  }
+  void f(int marker) {
+    System.out.println("f(" + marker + ")");
+  }
+}
+
+public class Mugs {
+  Mug c1;
+  Mug c2;
+  {
+    c1 = new Mug(1);
+    c2 = new Mug(2);
+    System.out.println("c1 & c2 initialized");
+  }
+  Mugs() {
+    System.out.println("Mugs()");
+  }
+  public static void main(String[] args) {
+    System.out.println("Inside main()");
+    Mugs x = new Mugs();
+  }
+} ///:~
+```
+
+###### 它看起来与静态初始化从句极其相似，只是static关键字从里面消失了。为支持对“匿名内部类”的初始化（参见第7章），必须采用这一语法格式。
+
+#### 4.5 数组初始化
+###### 数组属于引用数据类型，所以在数组使用之前一定要开辟控件（实例化），如果使用了没有开辟空间的数组，则一定会出现 NullPointerException 异常信息。既然数组属于引用数据类型，那么也一定可以发生引用传递。
+###### 数组先开辟内存空间，而后再使用索引进行内容的设置，实际上这种做法都叫做动态初始化，而如果希望数组在定义的时候可以同时出现设置内容，那么就可以采用静态初始化完成。
+```java
+        //动态初始化
+		int data[] = null;
+		data = new int[3]; //开辟一个长度为3的数组
+		int temp[] = null; //声明对象
+		data[0] = 10;
+		data[1] = 20;
+		data[2] = 30;
+        //静态初始化
+        int data[] = {1, 2, 4, 545, 11, 32, 13131, 4444};
+        int data2[] = new int[] {1, 2, 4, 545, 11, 32, 13131, 4444};
+```
+###### 所有数组都有一个本质成员（无论它们是对象数组还是基本类型数组），可对其进行查询——但不是改变，从而获知数组内包含了多少个元素。这个成员就是length。
+###### 基本数据类型的数组元素会自动初始化成“空”值（对于数值，空值就是零；对于char，它是null；而对于boolean，它却是false）。
+
+
+
+
+
+
+
+
 
 
 
