d6776b8cb74715b00cc701cb86668e60342d0dad
Shiliang <shiliang1992@outlook.com>
2020-03-04 19:01:29 +0800

    202034190123

--- html/technology/mark.mk
+++ html/technology/mark.mk
@@ -10,7 +10,13 @@
 2. BitTorrent Sync
 3. Sublime Text
 4. DBeaver
+    1. 一种开源的数据库可视管理工具
 5. CertUtil -hashfile C:\xxx.tar MD5
     1. 此命令不仅可以做MD5哈希算法校验，还支持其他的哈希算法，具体如下：
     2. CertUtil -hashfile 文件路径 [算法]
     3. 支持的算法有：MD2 MD4 MD5 SHA1 SHA256 SHA384 SHA512
+6. Go 编写的静态网站生成器 Hugo 0.64.1 发布
+7. ckeditor5
+    1. 强大的富文本编辑器框架，具有模块化体系结构，现代化的集成和协作编辑等功能。
+8. Atom
+    1. Atom 是github专门为程序员推出的一个跨平台文本编辑器。
