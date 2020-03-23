b39c0dcb554a0d22d38db12d10ccc48251efa2e2
shiliang <Shiliang.1995>
2020-03-23 16:27:48 +0800

    1584952052000

--- springbootTest/src/main/java/com/nostyling/create/util/test/logsearch/LogSearch.java
+++ springbootTest/src/main/java/com/nostyling/create/util/test/logsearch/LogSearch.java
@@ -1,13 +1,8 @@
 package com.nostyling.create.util.test.logsearch;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
 import java.util.HashMap;
 
 /**
@@ -19,14 +14,9 @@ import java.util.HashMap;
 public class LogSearch {
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
-        // 被搜索的文本
-        //String [] textToFind = {"1195269991111208961","1195269860609634306","1195246027999686658"};
-        String[] textToFind = {"取消发货,请求URL"};
-        //String [] textToFind = {"700580641688476236"};
 
-        // 被搜索的文件
-        String filepath = "C:\\Users\\shiliang\\Desktop\\root.log";
-        //String filepath = "C:\\Users\\shiliang\\Desktop\\root.log";
+
+        String filepath = "C:\\Users\\shiliang\\Desktop\\淘宝-天猫发货失败.txt";
 
         // 文件编码
         String encoding = "utf-8";
@@ -38,33 +28,26 @@ public class LogSearch {
         BufferedReader reader = new BufferedReader(new InputStreamReader(fis, encoding), inputBufferSize);
 
         Long countLine = 0L;
-        ArrayList <HashMap> successList = new ArrayList <>();
+        HashMap <String, String> success = new HashMap <>();
 
         String line = "";
         System.out.println("======开始=====");
         while ((line = reader.readLine()) != null) {
             countLine++;
-            for (String string : textToFind) {
-                boolean contains = line.contains(string);
-                if (contains) {
-                    HashMap <String, String> success = new HashMap <>();
 
-                    success.put("关键字", string);
-                    success.put("内容", String.valueOf(countLine));
-                    success.put("日志内容", line);
-                    successList.add(success);
-                }
+            if (null != line) {
+                String[] split = line.split(" ");
+                success.put(split[1], split[3]);
             }
 
+
         }
+
+
         System.out.println("======================");
         System.out.println("文本总行数：" + countLine);
-        System.out.println("共搜索到：" + successList.size() + " 处。");
-        for (HashMap <String, String> success : successList) {
-
-            System.out.println(JSON.toJSONString(success));
-            System.out.println();
-        }
+        System.out.println("共搜索到：" + success.size() + " 处。");
+        System.out.println(JSONObject.toJSONString(success));
         System.out.println("======================");
         System.out.println("======结束=====");
         long end = System.currentTimeMillis();
