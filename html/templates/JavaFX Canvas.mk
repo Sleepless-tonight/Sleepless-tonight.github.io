3ad608af985150db98d2dfc5fa6543c9aa266e71
shiliang <921789348@qq.com>
2020-04-30 04:41:23 +0800

    1588192820700

--- html/templates/JavaFX Canvas.mk
+++ html/templates/JavaFX Canvas.mk
@@ -1,2 +1,34 @@
 # 使用 Canvas API
 ### 总览
+JavaFX Canvas API提供了可以写入的自定义纹理。它是由类中定义Canvas并GraphicsContext在javafx.scene.canvas包中。使用此API涉及创建一个Canvas对象，获取其对象，GraphicsContext并调用绘图操作以在屏幕上呈现您的自定义形状。因为Canvas是Node子类，所以可以在JavaFX场景图中使用它。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
