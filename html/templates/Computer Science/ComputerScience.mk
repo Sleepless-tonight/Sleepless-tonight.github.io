c404e1bb4d67708599acb2e9e3bfa3ae0dcdf4c8
Shiliang <shiliang1992@outlook.com>
2020-08-07 18:56:35 +0800

    2020年8月6日18点5633分

--- html/templates/Computer Science/ComputerScience.mk
+++ html/templates/Computer Science/ComputerScience.mk
@@ -1,5 +1,23 @@
 # Computer Science
 
-1. 标签：计算机科学、《数据结构》、《计算机网络》、《计算机组成原理》、《计算机操作系统》
+1. 标签：计算机科学、《数据结构》、《计算机网络》、《计算机组成原理》、《计算机操作系统》、《离散数学》、《线性代数》、《编译原理》、数据库原理、软件工程、软件系统设计
 2. 时间：2020年8月7日17:33:00
 3. 连接：http://www.nostyling.cn/html/templates/Thinking-In-Java.mk
+
+
+
+[清华课程表](https://www.tsinghua.edu.cn/jyjx1/bksjy/bkzy1.htm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ html/templates/文件管理.mk
@@ -0,0 +1,8 @@
+
+
+
+![image](https://nostyling-1256016577.cos.ap-beijing.myqcloud.com/640%20%281%29.webp)
+
+![image](https://nostyling-1256016577.cos.ap-beijing.myqcloud.com/640.png)
+
+![image](https://nostyling-1256016577.cos.ap-beijing.myqcloud.com/640.webp)
